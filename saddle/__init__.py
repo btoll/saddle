5442986f9dedefc4a23dc94985cf13b62854a213
Benjamin Toll <benjam72@yahoo.com>
2020-11-13 23:11:15 -0500

    Going down the ole FP rabbit hole

--- saddle/__init__.py
+++ saddle/__init__.py
@@ -14,6 +14,24 @@ import yaml
 # we have to do some futzing with the results to work with them.
 # This is an area that will change.
 
+def partial(fn, *args):
+    def wrap(*inner):
+        return fn(*args, *inner)
+    return wrap
+
+
+def compose(f, g):
+    def wrap(*args):
+        return f(g(*args))
+    return wrap
+
+
+#def compose3(f, g, h):
+#    def wrap(*args):
+#        return f(g(h(*args)))
+#    return wrap
+
+
 def _charset(charset):
     def decoder(f):
         @functools.wraps(f)
@@ -24,24 +42,33 @@ def _charset(charset):
     return decoder
 
 
-def _validate_list_choice(f):
+def _get_list_item(f):
     @functools.wraps(f)
     def wrap(*args):
-        (choice, filename) = f(*args)
-        items = args[0]
+        items = args[1]
+        choice = f(*args)
+        return choice, items[choice]
+    return wrap
+
+
+def _validate(f):
+    @functools.wraps(f)
+    def wrap(*args):
+        items = args[1]
+        choice = f(*args)
         if -1 < choice < len(items):
-            return (choice, filename)
+            return choice
         else:
             raise ValueError("Out of range")
     return wrap
 
 
-@_validate_list_choice
-def choose_input(items, name):
+@_get_list_item
+@_validate
+def choose_input(name, items):
     for item in list_items(items):
         print(item)
-    choice = int(input(f"\nChoose {name}: ")) - 1
-    return (choice, items[choice])
+    return int(input(f"\nChoose {name}: ")) - 1
 
 
 def create_outfile(state, filename, fn):
@@ -64,6 +91,13 @@ def get_cmd_results(cmd):
     return run_cmd(cmd).strip()
 
 
+def get_files():
+    yamls = list(filter(magic_number, glob.glob("*.yaml")))
+    if not len(yamls):
+        raise Exception(f"No `mule` files found in {os.getcwd()}>")
+    return yamls
+
+
 def get_job_env(mule_yaml, mule_job):
     return get_cmd_results(["mule", "-f", mule_yaml, "--list-env", mule_job, "--verbose"])
 
@@ -75,10 +109,6 @@ def get_jobs(mule_yaml):
 
 def get_json(o):
     return json.dumps(o, sort_keys=True, indent=4)
-
-
-def get_mule_yamls():
-    return list(filter(magic_number, glob.glob("*.yaml")))
 
 
 def get_state(mule_agent):
@@ -110,7 +140,7 @@ def get_yaml(o):
 
 
 def list_items(items):
-    return ["{}) {}".format(index + 1, item) for index, item in enumerate(items)]
+    return ["{}) {}".format(idx + 1, item) for idx, item in enumerate(items)]
 
 
 def magic_number(filename):
@@ -138,40 +168,44 @@ extensions = {
 }
 
 
+choose_file = partial(choose_input, "file")
+choose_job = partial(choose_input, "job")
+choose_operation = partial(choose_input, "operation")
+
+
+get_mule_yaml = compose(choose_file, get_files)
+get_mule_job = compose(choose_job, get_jobs)
+
+
 def main():
-    yamls = get_mule_yamls()
-    if len(yamls):
-        (_, mule_yaml) = choose_input(yamls, "file")
-        jobs = get_jobs(mule_yaml)
-        (_, mule_job) = choose_input(jobs, "job")
-        mule_agents = get_agents(mule_yaml)
-        mule_state = []
-        if len(mule_agents):
-            mule_job_env = get_job_env(mule_yaml, mule_job)
-            print(f"\nEnv found in {mule_job}:\n")
-            mule_state = get_state(mule_job_env)
-        (index, _) = choose_input(["Run", "Save as recipe"], "operation")
-        if index == 0:
-            print(" ".join(mule_state + [f"mule -f {mule_yaml} {mule_job}"]))
-        else:
-            filename = input(f"\nName of outfile: ")
-            [basename, ext] = os.path.splitext(filename)
-            state = {
-                "created": str(datetime.datetime.now()),
-                "version": get_cmd_results(["mule", "-v"]),
-                "file": "/".join((os.getcwd(), mule_yaml)),
-                "job": mule_job,
-                "env": mule_state
-            }
-            if filename.endswith(".*"):
-                for (ext, fn) in extensions.items():
-                    create_outfile(state, "".join((basename, ext)), fn)
-            else:
-                # Default to yaml if there is an unrecognized file extension
-                # or if there is no file extension.
-                create_outfile(state, filename, extensions.get(ext, get_yaml))
+    _, mule_yaml = get_mule_yaml()
+    _, mule_job = get_mule_job(mule_yaml)
+    mule_agents = get_agents(mule_yaml)
+    mule_state = []
+    if len(mule_agents):
+        mule_job_env = get_job_env(mule_yaml, mule_job)
+        print(f"\nEnv found in {mule_job}:\n")
+        mule_state = get_state(mule_job_env)
+    idx, _ = choose_operation(["Run", "Save as recipe"])
+    if idx == 0:
+        print(" ".join(mule_state + [f"mule -f {mule_yaml} {mule_job}"]))
     else:
-        print(f"No `mule` files found in {os.getcwd()}.")
+        filename = input(f"\nName of outfile: ")
+        [basename, ext] = os.path.splitext(filename)
+        state = {
+            "created": str(datetime.datetime.now()),
+            "version": get_cmd_results(["mule", "-v"]),
+            "file": "/".join((os.getcwd(), mule_yaml)),
+            "job": mule_job,
+            "env": mule_state
+        }
+        if filename.endswith(".*"):
+            for (ext, fn) in extensions.items():
+                create_outfile(state, "".join((basename, ext)), fn)
+        else:
+            # Default to yaml if there is an unrecognized file extension
+            # or if there is no file extension.
+            create_outfile(state, filename, extensions.get(ext, get_yaml))
 
 
 if __name__ == "__main__":
