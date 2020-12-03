a041a41ecd8c54815a67d2935d224abbc14a548c
Benjamin Toll <benjam72@yahoo.com>
2020-12-03 00:49:36 -0500

    Use console_scripts entry point

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 from setuptools import setup
 
-setup(name="saddle",
+setup(
+    name="saddle",
     version="0.1",
-    scripts=["bin/saddle"],
-    description="Saddle up, for tonight we ride!",
+    description="Saddle up, tonight we ride!",
     url="http://github.com/btoll/saddle",
     author="btoll",
     author_email="benjam72@yahoo.com",
@@ -11,8 +11,6 @@ setup(name="saddle",
     packages=["saddle"],
     include_package_data=True,
     install_requires=[
-        "json",
-        "mulecli",
         "pyyaml",
     ],
     classifiers=[
@@ -22,5 +20,11 @@ setup(name="saddle",
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.7",
     ],
-    zip_safe=False)
+    zip_safe=False,
+    entry_points={
+        "console_scripts": [
+            "saddle = saddle:main"
+        ]
+    }
+)
 
