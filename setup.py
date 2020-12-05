0f1be62c29dd7964a7957292c7b1e1b6c83381b7
Benjamin Toll <benjam72@yahoo.com>
2020-12-05 14:34:48 -0500

    Improve package tooling

--- setup.py
+++ setup.py
@@ -1,30 +1,4 @@
-from setuptools import setup
+import setuptools
 
-setup(
-    name="saddle",
-    version="0.1",
-    description="Saddle up, tonight we ride!",
-    url="http://github.com/btoll/saddle",
-    author="btoll",
-    author_email="benjam72@yahoo.com",
-    license="GPLv3+",
-    packages=["saddle"],
-    include_package_data=True,
-    install_requires=[
-        "pyyaml",
-    ],
-    classifiers=[
-        # https://pypi.org/pypi?%3Aaction=list_classifiers
-        "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)"
-        "Programming Language :: Python",
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.7",
-    ],
-    zip_safe=False,
-    entry_points={
-        "console_scripts": [
-            "saddle = saddle:main"
-        ]
-    }
-)
+setuptools.setup()
 
